712440398fdde3dfe03f0e1b29d26b5bae2a5af5
Rekurt-116 <sabrlitet@gmail.com>
2024-12-11 16:22:11 +0400

    подключение Angular material + Стилизация

--- src/app/components/create-todo-form/create-todo-form.ts
+++ src/app/components/create-todo-form/create-todo-form.ts
@@ -1,12 +1,16 @@
 import { Component, EventEmitter, Output } from "@angular/core";
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import {  MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
     selector: 'app-create-todo-form',
     standalone: true,
     templateUrl: './create-todo-form.html',
     styleUrl: './create-todo-form.scss',
-    imports: [ReactiveFormsModule],
+    imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
 })
 export class CreateTodoFormComponent {
 
@@ -16,7 +20,7 @@ export class CreateTodoFormComponent {
 
     public form = new FormGroup({
        userId: new FormControl(null,[Validators.required, Validators.minLength(1)]),
-       id: new FormControl(null,[Validators.required, Validators.minLength(1)]),
+       id: new FormControl(),
        title: new FormControl(null,[Validators.required, Validators.minLength(1)]),
        completed: new FormControl(null,[Validators.required, Validators.minLength(1)]) 
     });
