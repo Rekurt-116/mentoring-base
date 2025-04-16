1e1f4b29c6f1b9cd585fed3b50e6e3928040374c
MadAngular <roro555000ro@gmail.com>
2025-04-16 16:53:08 +0300

    add material in users

--- src/app/components/forms/user-form/user-form.component.ts
+++ src/app/components/forms/user-form/user-form.component.ts
@@ -12,21 +12,27 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
+
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.scss',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
 })
 export class UserFormComponent {
-  
   @Output()
   createUser = new EventEmitter();
 
   form: FormGroup;
   constructor(private formBuilder: FormBuilder) {
+    
     this.form = this.formBuilder.group({
       name: [null, [Validators.required]],
       email: [null, [Validators.required]],
