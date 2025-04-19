4ee89246f37f782973fb41c96c0589acb03c2f15
MadAngular <roro555000ro@gmail.com>
2025-04-19 14:03:02 +0300

    Update min-lenght-pipe.ts

--- src/app/pipes/min-lenght-pipe.ts
+++ src/app/pipes/min-lenght-pipe.ts
@@ -9,7 +9,7 @@ export class MinLengthPipe implements PipeTransform{
     transform(text: string): string {
      if(text.length < 20){
         return text;
-     }return text.substring(0, 20);
+     }return text.substring(0, 21);
     }
    
 }
